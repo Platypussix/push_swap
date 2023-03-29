346fe04ea3cb46947dfbd20eaff26f5afc4bbf99
Armand MEDIOUNA <amedioun@e3r2p18.clusters.42paris.fr>
2023-03-29 16:56:20 +0200

    commit auto de la flemme absolue

--- src/simple_sort.c
+++ src/simple_sort.c
@@ -6,7 +6,7 @@
 /*   By: amedioun <amedioun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:20:50 by amedioun          #+#    #+#             */
-/*   Updated: 2023/03/10 16:06:39 by amedioun         ###   ########.fr       */
+/*   Updated: 2023/03/29 16:52:06 by amedioun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,54 +64,79 @@ static void	sort_3(t_list **stack)
 	}
 }
 
-static void	sort_4(t_list **stack_a, t_list **stack_b)
-{
-	int	distance;
+// static void	sort_4(t_list **stack_a, t_list **stack_b)
+// {
+// 	int	distance;
 
-	if (is_sorted(stack_a))
-		return ;
-	distance = get_distance(stack_a, get_min(stack_a, -1));
-	if (distance == 1)
-		ra(stack_a);
-	else if (distance == 2)
-	{
-		ra(stack_a);
-		ra(stack_a);
-	}
-	else if (distance == 3)
-		rra(stack_a);
-	if (is_sorted(stack_a))
-		return ;
-	pb(stack_a, stack_b);
-	sort_3(stack_a);
-	pa(stack_a, stack_b);
-}
+// 	if (is_sorted(stack_a))
+// 		return ;
+// 	distance = get_distance(stack_a, get_min(stack_a, -1));
+// 	if (distance == 1)
+// 		ra(stack_a);
+// 	else if (distance == 2)
+// 	{
+// 		ra(stack_a);
+// 		ra(stack_a);
+// 	}
+// 	else if (distance == 3)
+// 		rra(stack_a);
+// 	if (is_sorted(stack_a))
+// 		return ;
+// 	pb(stack_a, stack_b);
+// 	sort_3(stack_a);
+// 	pa(stack_a, stack_b);
+// }
+
+// static void	sort_5(t_list **stack_a, t_list **stack_b)
+// {
+// 	int	distance;
+
+// 	if (is_sorted(stack_a))
+// 		return ;
+// 	distance = get_distance(stack_a, get_min(stack_a, -1));
+// 	if (distance == 1)
+// 		ra(stack_a);
+// 	else if (distance == 2)
+// 	{
+// 		ra(stack_a);
+// 		ra(stack_a);
+// 	}
+// 	else if (distance == 3)
+// 	{
+// 		rra(stack_a);
+// 		rra(stack_a);
+// 	}
+// 	else if (distance == 4)
+// 		rra(stack_a);
+// 	if (is_sorted(stack_a))
+// 		return ;
+// 	pb(stack_a, stack_b);
+// 	sort_4(stack_a, stack_b);
+// 	pa(stack_a, stack_b);
+// }
 
 static void	sort_5(t_list **stack_a, t_list **stack_b)
 {
-	int	distance;
+	t_list	*head;
+	int		min;
+	int		i;
 
+	min = get_min(stack_a, 0);
+	head = *stack_a;
+	i = 0;
 	if (is_sorted(stack_a))
 		return ;
-	distance = get_distance(stack_a, get_min(stack_a, -1));
-	if (distance == 1)
-		ra(stack_a);
-	else if (distance == 2)
+	while (i++ < 2)
 	{
-		ra(stack_a);
-		ra(stack_a);
+		while (head->index != min)
+			head = head->next;
+		pb(stack_a, stack_b);
 	}
-	else if (distance == 3)
-	{
-		rra(stack_a);
-		rra(stack_a);
-	}
-	else if (distance == 4)
-		rra(stack_a);
-	if (is_sorted(stack_a))
-		return ;
-	pb(stack_a, stack_b);
-	sort_4(stack_a, stack_b);
+	printf("%d", head->value);
+	printf("%d", head->next->value);
+	printf("%d", head->next->next->value);
+	sort_3(stack_a);
+	pa(stack_a, stack_b);
 	pa(stack_a, stack_b);
 }
 
@@ -126,8 +151,8 @@ void	simple_sort(t_list **stack_a, t_list **stack_b)
 		sa(stack_a);
 	else if (size == 3)
 		sort_3(stack_a);
-	else if (size == 4)
-		sort_4(stack_a, stack_b);
+//	else if (size == 4)
+//		sort_4(stack_a, stack_b);
 	else if (size == 5)
 		sort_5(stack_a, stack_b);
 }
